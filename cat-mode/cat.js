9f7553b559345ac0ab1e89fd8d8c9064b70fac71
Jacky Wu <JackyWuCoder@gmail.com>
2025-04-07 15:23:12 -0700

    feat: render first frame of idle cat sprite on canvas

--- cat-mode/cat.js
+++ cat-mode/cat.js
@@ -4,4 +4,12 @@ export function setupCat(canvasId = "cat-canvas") {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+    const idleImg = new Image();
+    idleImg.src = "./sprites/cat_idle.png";
+
+    idleImg.onload = () => {
+        ctx.drawImage(idleImg, 0, 0, 32, 32, 100, 100, 32, 32);
+    }    
 }

627af6aa789dff953ab43f5004d4313e91f0bae4
Jacky Wu <JackyWuCoder@gmail.com>
2025-04-07 21:03:07 -0700

    feat: toggle light/dark mode using "dark-mode" class name

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    toggleButton.textContent = body.classList.contains("dark-mode") ? "🌞" : "🌚";
+})
